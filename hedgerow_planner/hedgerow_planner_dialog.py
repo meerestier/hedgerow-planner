fe654d7d17689905189275d8d0e4984d2feb62f7
meerestier <lars.schulz@systemic.de>
2024-06-27 10:53:20 +0200

    Initial commit

--- hedgerow_planner/hedgerow_planner_dialog.py
+++ hedgerow_planner/hedgerow_planner_dialog.py
@@ -22,23 +22,16 @@
  ***************************************************************************/
 """
 
+from PyQt5 import uic
+from PyQt5.QtWidgets import QDialog
 import os
 
-from qgis.PyQt import uic
-from qgis.PyQt import QtWidgets
-
-# This loads your .ui file so that PyQt can populate your plugin with the elements from Qt Designer
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'hedgerow_planner_dialog_base.ui'))
 
 
-class hedgerow_plannerDialog(QtWidgets.QDialog, FORM_CLASS):
+class HedgerowPlannerDialog(QDialog, FORM_CLASS):
     def __init__(self, parent=None):
         """Constructor."""
-        super(hedgerow_plannerDialog, self).__init__(parent)
-        # Set up the user interface from Designer through FORM_CLASS.
-        # After self.setupUi() you can access any designer object by doing
-        # self.<objectname>, and you can use autoconnect slots - see
-        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
-        # #widgets-and-dialogs-with-auto-connect
+        super(HedgerowPlannerDialog, self).__init__(parent)
         self.setupUi(self)
